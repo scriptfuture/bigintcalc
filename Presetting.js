8dd8a9730b9764f7b5274ee4e5dff07ba3386873
scriptfuture <progratronic@gmal.com>
2013-08-18 03:49:05 +0400

    The finished version 1.0

--- Presetting.js
+++ Presetting.js
@@ -1,4 +1,40 @@
- // JavaScript Document
+/*
+    Copyright 2013 Решетников Николай (Reshetnikov Nikolay)
+
+    This file is part of BigIntCalc.
+
+    BigIntCalc is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    BigIntCalc is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+  (Этот файл — часть BigIntCalc.
+                           
+   BigIntCalc - свободная программа: вы можете перераспространять ее и/или
+   изменять ее на условиях Стандартной общественной лицензии GNU в том виде,
+   в каком она была опубликована Фондом свободного программного обеспечения;
+   либо версии 3 лицензии, либо (по вашему выбору) любой более поздней
+   версии.
+
+   BigIntCalc распространяется в надежде, что она будет полезной,
+   но БЕЗО ВСЯКИХ ГАРАНТИЙ; даже без неявной гарантии ТОВАРНОГО ВИДА
+   или ПРИГОДНОСТИ ДЛЯ ОПРЕДЕЛЕННЫХ ЦЕЛЕЙ. Подробнее см. в Стандартной
+   общественной лицензии GNU.
+
+   Вы должны были получить копию Стандартной общественной лицензии GNU
+   вместе с этой программой. Если это не так, см.
+   <http://www.gnu.org/licenses/>.)
+*/
+
+
  var resultSpan;
 
  window.onload = function () {
@@ -47,12 +83,12 @@
 
      var optionSubtract = document.createElement("option");
      optionSubtract.value = "subtract";
-     optionSubtract.text = "вычетание";
+     optionSubtract.text = "вычитание";
      selectAction.add(optionSubtract);
 
      var optionMultiply = document.createElement("option");
      optionMultiply.value = "multiply";
-     optionMultiply.text = "умнажение";
+     optionMultiply.text = "умножение";
      selectAction.add(optionMultiply);
 
      var optionDivide = document.createElement("option");
@@ -249,7 +285,6 @@
      option2to16.value = "2to16";
      option2to16.text = "преобразовать двоичное число в 16-тиричное";
      selectActionOne.add(option2to16);
-
 
      maindiv.appendChild(selectActionOne);
 
