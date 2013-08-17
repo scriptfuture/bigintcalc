dccd1714bab53ab72fa25214319f6894bc7d2464
scriptfuture <progratronic@gmal.com>
2013-08-18 03:14:37 +0400

    first commit

---
+++ Presetting.js
@@ -0,0 +1,326 @@
+ // JavaScript Document
+var resultSpan; 
+ 
+window.onload = function(){
+
+    // настройки страницы
+    document.body.style.margin="0px";
+    document.body.style.padding="0px";
+    document.body.style.backgroundColor="#f5f5f7";
+    document.body.style.fontFamily="Tahoma, Geneva, Kalimati, sans-serif";
+
+    // основной слой
+    var maindiv=document.createElement('div');
+    maindiv.style.margin="0 auto";
+    maindiv.style.width="800px";
+    maindiv.style.height="100%";
+    maindiv.style.padding="15px";
+    maindiv.style.backgroundColor="#fdfdfe";
+    maindiv.style.borderLeft="4px double navy";
+    maindiv.style.borderRight="4px double navy";
+    maindiv.style.borderBottom="4px double navy";
+    maindiv.style.fontSize="18px";
+    maindiv.style.color="navy"; 
+
+    /**** действия над двумя операндами ****/
+    var strtext=document.createTextNode('Калькулятор и конвертер большых чисел On-Line');
+    maindiv.appendChild(strtext);
+    
+    // перенос 
+    maindiv.appendChild(document.createElement('br'));
+    maindiv.appendChild(document.createElement('br'));
+    
+    var selectAction=document.createElement('select');
+        selectAction.style.fontSize="18px";
+        selectAction.style.color="navy"; 
+    
+var option=document.createElement("option");
+option.selected="selected";
+option.disabled="disabled";
+option.text="выбирите действие";
+selectAction.add(option);
+
+var optionAdd=document.createElement("option");
+optionAdd.value="add";
+optionAdd.text="сложение"; 
+selectAction.add(optionAdd);
+
+var optionSubtract=document.createElement("option");
+optionSubtract.value="subtract";
+optionSubtract.text="вычетание"; 
+selectAction.add(optionSubtract);
+
+var optionMultiply=document.createElement("option");
+optionMultiply.value="multiply";
+optionMultiply.text="умнажение"; 
+selectAction.add(optionMultiply);
+
+var optionDivide=document.createElement("option");
+optionDivide.value="divide";
+optionDivide.text="деление"; 
+selectAction.add(optionDivide);
+
+var optionMod=document.createElement("option");
+optionMod.value="mod";
+optionMod.text="остаток от деления"; 
+selectAction.add(optionMod);
+
+var optionPow=document.createElement("option");
+optionPow.value="pow";
+optionPow.text="возведение в степень"; 
+selectAction.add(optionPow);
+
+var optionCompare=document.createElement("option");
+optionCompare.value="compare";
+optionCompare.text="сравнение"; 
+selectAction.add(optionCompare);
+
+ maindiv.appendChild(selectAction);
+
+    // перенос 
+    maindiv.appendChild(document.createElement('br'));
+    maindiv.appendChild(document.createElement('br'));
+    
+    var aInput=document.createElement('input');
+    aInput.style.width="100%";
+    aInput.style.fontSize="18px";
+    aInput.style.color="silver"; 
+    aInput.value="первое число"; 
+    aInput.onclick=function(e){
+        if(this.value=="первое число") { 
+            this.value=""; 
+            this.style.color="navy"; 
+        } // end if
+      } // end fun
+    maindiv.appendChild(aInput);
+    
+    
+    // перенос 
+    maindiv.appendChild(document.createElement('br'));
+    maindiv.appendChild(document.createElement('br'));
+    
+     var bInput=document.createElement('input');
+     bInput.style.width="100%";
+        bInput.style.fontSize="18px";
+        bInput.style.color="silver"; 
+        bInput.value="второе число";
+   bInput.onclick=function(e){
+     
+    if(this.value=="второе число") { 
+        this.value=""; 
+        this.style.color="navy";
+    }
+     
+    } // end fun 
+       maindiv.appendChild(bInput);
+       
+       
+    // перенос 
+    maindiv.appendChild(document.createElement('br'));
+    maindiv.appendChild(document.createElement('br'));
+
+     var countUpInput=document.createElement('input');
+        countUpInput.style.fontSize="18px";
+        countUpInput.style.color="#483D8B";
+        countUpInput.type="button"; 
+        countUpInput.value="посчитать";
+   countUpInput.onclick=function(e){
+   
+      if(!isNaN(aInput.value) && !isNaN(bInput.value)) {
+         calculate(aInput.value, bInput.value,selectAction.value);
+      } else {
+         alert("Одно или оба значения не являются числами!");
+      } // end if
+   
+   
+     
+    } // end fun 
+       maindiv.appendChild(countUpInput);
+       
+    // перенос 
+    maindiv.appendChild(document.createElement('br'));
+    maindiv.appendChild(document.createElement('br'));
+    
+    resultSpan=document.createElement('span');
+    resultSpan.style.width="100%";
+    resultSpan.style.fontSize="18px";
+    resultSpan.style.color="navy";
+    maindiv.appendChild(resultSpan);
+    
+    // перенос 
+    maindiv.appendChild(document.createElement('br'));
+    maindiv.appendChild(document.createElement('br'));
+    
+    /**** действия над одним операндом ****/
+    
+    var strtextOne=document.createTextNode('С одним числом');
+    maindiv.appendChild(strtextOne);
+    
+    // перенос 
+    maindiv.appendChild(document.createElement('br'));
+    maindiv.appendChild(document.createElement('br'));
+    
+    var selectActionOne=document.createElement('select');
+        selectActionOne.style.fontSize="18px";
+        selectActionOne.style.color="navy"; 
+    
+var optionOne=document.createElement("option");
+optionOne.selected="selected";
+optionOne.disabled="disabled";
+optionOne.text="выбирите действие";
+selectActionOne.add(optionOne);
+
+var optionNegate=document.createElement("option");
+optionNegate.value="negate";
+optionNegate.text="отрицание десятичного числа"; 
+selectActionOne.add(optionNegate);
+
+var optionAbs=document.createElement("option");
+optionAbs.value="abs";
+optionAbs.text="модуль десятичного числа"; 
+selectActionOne.add(optionAbs);
+
+var optionIntPart=document.createElement("option");
+optionIntPart.value="intPart";
+optionIntPart.text="целая часть десятичного числа"; 
+selectActionOne.add(optionIntPart);
+
+var optionRevers=document.createElement("option");
+optionRevers.value="revers";
+optionRevers.text="реверс числа"; 
+selectActionOne.add(optionRevers);
+
+var option10to2=document.createElement("option");
+option10to2.value="10to2";
+option10to2.text="преобразовать десятичное число в двоичное"; 
+selectActionOne.add(option10to2);
+
+var option10to8=document.createElement("option");
+option10to8.value="10to8";
+option10to8.text="преобразовать десятичное число в 8-миричное"; 
+selectActionOne.add(option10to8);
+
+var option10to16=document.createElement("option");
+option10to16.value="10to16";
+option10to16.text="преобразовать десятичное число в 16-тиричное"; 
+selectActionOne.add(option10to16);
+
+var option16to2=document.createElement("option");
+option16to2.value="16to2";
+option16to2.text="преобразовать 16-тиричное число в двоичное"; 
+selectActionOne.add(option16to2);
+
+var option16to8=document.createElement("option");
+option16to8.value="16to8";
+option16to8.text="преобразовать 16-тиричное число в 8-миричное"; 
+selectActionOne.add(option16to8);
+
+var option16to10=document.createElement("option");
+option16to10.value="16to10";
+option16to10.text="преобразовать 16-тиричное число в десятичное"; 
+selectActionOne.add(option16to10);
+
+var option8to2=document.createElement("option");
+option8to2.value="8to2";
+option8to2.text="преобразовать 8-миричное число в двоичное"; 
+selectActionOne.add(option8to2);
+
+var option8to10=document.createElement("option");
+option8to10.value="8to10";
+option8to10.text="преобразовать 8-миричное число в десятичное"; 
+selectActionOne.add(option8to10);
+
+var option8to16=document.createElement("option");
+option8to16.value="8to16";
+option8to16.text="преобразовать 8-миричное число в 16-тиричное"; 
+selectActionOne.add(option8to16);
+
+var option2to8=document.createElement("option");
+option2to8.value="2to8";
+option2to8.text="преобразовать двоичное число в 8-миричное"; 
+selectActionOne.add(option2to8);
+
+var option2to10=document.createElement("option");
+option2to10.value="2to10";
+option2to10.text="преобразовать двоичное число в десятичное"; 
+selectActionOne.add(option2to10);
+
+var option2to16=document.createElement("option");
+option2to16.value="2to16";
+option2to16.text="преобразовать двоичное число в 16-тиричное"; 
+selectActionOne.add(option2to16);
+/*
+
+var option2toDC=document.createElement("option");
+option2toDC.value="2toDC";
+option2toDC.text="двоичное число в двоичный дополнительный код"; 
+selectActionOne.add(option2toDC);
+
+var option2toDC16=document.createElement("option");
+option2toDC16.value="2toDC16";
+option2toDC16.text="двоичное число в 16-тиричный дополнительный код"; 
+selectActionOne.add(option2toDC16);
+*/
+
+
+ maindiv.appendChild(selectActionOne);
+
+    // перенос 
+    maindiv.appendChild(document.createElement('br'));
+    maindiv.appendChild(document.createElement('br'));
+    
+    var aInputOne=document.createElement('input');
+    aInputOne.style.width="100%";
+    aInputOne.style.fontSize="18px";
+    aInputOne.style.color="silver"; 
+    aInputOne.value="введите число"; 
+    aInputOne.onclick=function(e){
+        if(this.value=="введите число") { 
+            this.value=""; 
+            this.style.color="navy"; 
+        } // end if
+      } // end fun
+    maindiv.appendChild(aInputOne);
+     
+       
+    // перенос 
+    maindiv.appendChild(document.createElement('br'));
+    maindiv.appendChild(document.createElement('br'));
+
+     var countUpInputOne=document.createElement('input');
+        countUpInputOne.style.fontSize="18px";
+        countUpInputOne.style.color="#483D8B";
+        countUpInputOne.type="button"; 
+        countUpInputOne.value="посчитать";
+   countUpInputOne.onclick=function(e){
+   
+      if(aInputOne.value!="") {
+         convert(aInputOne.value, selectActionOne.value);
+      } else {
+         alert("Значение не являются числом!");
+      } // end if
+   
+   
+     
+    } // end fun 
+       maindiv.appendChild(countUpInputOne);
+       
+    // перенос 
+    maindiv.appendChild(document.createElement('br'));
+    maindiv.appendChild(document.createElement('br'));
+    
+    resultSpanOne=document.createElement('span');
+    resultSpanOne.style.width="100%";
+    resultSpanOne.style.fontSize="18px";
+    resultSpanOne.style.color="navy";
+    maindiv.appendChild(resultSpanOne);
+
+
+    // добавление корневых слоёв на сцену
+    if (document.body.firstChild){
+        document.body.insertBefore(maindiv, document.body.firstChild);  
+    } else {
+	      document.body.appendChild(maindiv);
+    } // end if
+
+} // end fun
---
+++ Scripts.js
@@ -0,0 +1,378 @@
+/* Списки преобразований*/
+var symb16to2 = {
+    "h0" : "0000",  
+    "h1" : "0001", 
+    "h2" : "0010", 
+    "h3" : "0011", 
+    "h4" : "0100", 
+    "h5" : "0101", 
+    "h6" : "0110", 
+    "h7" : "0111", 
+    "h8" : "1000", 
+    "h9" : "1001", 
+    "ha" : "1010", 
+    "hb" : "1011", 
+    "hc" : "1100", 
+    "hd" : "1101", 
+    "he" : "1110", 
+    "hf" : "1111" 
+}
+
+var symb8to2 = {
+    "h0" : "000",  
+    "h1" : "001", 
+    "h2" : "010", 
+    "h3" : "011", 
+    "h4" : "100", 
+    "h5" : "101", 
+    "h6" : "110", 
+    "h7" : "111"
+}
+
+var symb2to8 = {
+    "000" : "0",  
+    "001" : "1", 
+    "010" : "2", 
+    "011" : "3", 
+    "100" : "4", 
+    "101" : "5", 
+    "110" : "6", 
+    "111" : "7"
+}
+
+var symb2to16 = {
+    "0000" : "0",  
+    "0001" : "1", 
+    "0010" : "2", 
+    "0011" : "3", 
+    "0100" : "4", 
+    "0101" : "5", 
+    "0110" : "6", 
+    "0111" : "7", 
+    "1000" : "8", 
+    "1001" : "9", 
+    "1010" : "A", 
+    "1011" : "B", 
+    "1100" : "C", 
+    "1101" : "D", 
+    "1110" : "E", 
+    "1111" : "F" 
+}
+
+var symbTo10 = {
+    "0" : "0",  
+    "1" : "1", 
+    "2" : "2", 
+    "3" : "3", 
+    "4" : "4", 
+    "5" : "5", 
+    "6" : "6", 
+    "7" : "7", 
+    "8" : "8", 
+    "9" : "9", 
+    "a" : "10", 
+    "b" : "11", 
+    "c" : "12", 
+    "d" : "13", 
+    "e" : "14", 
+    "f" : "15" 
+}
+
+/* функции преобразований */
+function calculate(a,b,type) {
+
+var bigA = new BigNumber(a);
+var result;
+
+switch (type) {
+   case "add":
+      result = bigA.add(b);
+      break;
+   case "subtract":
+      result = bigA.subtract(b);
+      break;
+   case "multiply":
+      result = bigA.multiply(b);
+      break;
+   case "divide":
+      result = bigA.divide(b);
+      break;
+   case "mod":
+      result = bigA.mod(b);
+      break;
+   case "pow":
+      result = bigA.pow(b);
+      break;
+   case "compare":
+      result = bigA.compare(b);
+      break;
+   default:
+     alert("Выбирите действие!"); 
+      break;
+}
+
+resultSpan.innerHTML="Результат:<br>"+result;
+
+} // end fun
+
+function convert(a,type) {
+
+var bigA = new BigNumber(a);
+var result;
+
+switch (type) {
+   case "negate":
+      result = bigA.negate();
+      break;
+   case "abs":
+      result = bigA.abs();
+      break;
+   case "intPart":
+      result = bigA.intPart();
+      break;
+   case "revers":
+      result = a.split("").reverse().join("");
+      break;
+   case "10to2":
+      result=convertNumber10to2(bigA,"");
+      break;
+   case "10to8":
+      result = convertNumber10to2(bigA,"");
+      result = convertNumber2to8(result);
+      break;
+   case "10to16":
+      result = convertNumber10to2(bigA,"");
+      result = convertNumber2to16(result);
+      break;
+   case "16to2":
+      result = convertNumber16to2(a);
+      break;
+   case "16to8":
+      result = convertNumber16to2(a);
+      result = convertNumber2to8(result);
+      break;
+   case "16to10":
+      result = convertNumberMaxTo10(a,16);
+      break;
+   case "2to8":
+      result = convertNumber2to8(a);
+      break;
+   case "2to10":
+      result = convertNumber2to16(a);
+      result = convertNumberMaxTo10(result,16);
+      break;
+   case "2to16":
+      result = convertNumber2to16(a);
+      break;
+   case "8to2":
+      result = convertNumber8to2(a);
+      break;
+   case "8to10":
+      result = convertNumber8to2(a);
+      result = convertNumber2to16(result);
+      result = convertNumberMaxTo10(result,16);
+      break;
+   case "8to16":
+      result = convertNumber8to2(a);
+      result = convertNumber2to16(result);
+      break;
+   default:
+     alert("Выбирите действие!"); 
+      break;
+}
+
+resultSpanOne.innerHTML="Результат:<br>"+result;
+
+} //  end function  
+
+function convertNumber10to2(bigA,res){
+
+    var mod = bigA.mod(2);
+    var osn = bigA.divide(2).subtract(mod.divide(2)).intPart();
+    
+    res+=mod;
+    
+    if(osn>0) { 
+        return convertNumber10to2(osn,res);
+    } else {
+        return res.split("").reverse().join("");
+    }
+}
+
+
+function convertNumberMaxTo10(a,src_ss){
+
+    var sla = a.toLowerCase().split("").reverse();
+
+    var result = new BigNumber("0");
+    for(var i in sla){
+
+        var sumbcode=sla[i];
+        var numsymb = new BigNumber(symbTo10[sumbcode]);
+        var stepen = new BigNumber(src_ss);
+         stepen = stepen.pow(i);
+        numsymb = numsymb.multiply(stepen);
+        result=result.add(numsymb);
+    } // end for
+  return result;
+}
+
+
+function convertNumber16to2(a){
+
+    var sla = a.toLowerCase();
+
+    var result="";
+    for(var i in sla){
+
+        var sumbcode='h'+sla[i];
+        result+=symb16to2[sumbcode];
+    } // end for
+  return result;
+}
+
+
+function convertNumber8to2(a){
+
+    var sla = a.toLowerCase();
+
+    var result="";
+    for(var i in sla){
+
+        var sumbcode='h'+sla[i];
+        result+=symb8to2[sumbcode];
+    } // end for
+  return result;
+}
+
+
+
+function convertNumber2to8(a){
+
+    var sla = a.toLowerCase();
+
+    var result="";
+    
+    var z=0;
+    var n=0;
+    var sybmRes=[];
+    var sakom="";
+    for(var i in sla){
+    
+        if(z==2) {  
+            sakom+=sla[i]; sybmRes[n]=sakom; sakom="";  z=0; n++; 
+        } else { 
+            sakom+=sla[i]; z++;
+            if(i==sla.length-1) { sybmRes[n]=sakom; }  
+        } 
+    } // end for  
+    
+    if(z==1 || z==2) {
+    
+       // из массива в строку
+       sybmRes=sybmRes.join(''); 
+    
+       if(z==1) { sybmRes="00"+sybmRes; }
+       if(z==2) { sybmRes="0"+sybmRes; }
+    
+        var z2=0;
+        var n2=0;
+        var sybmRes2=[];
+        var sakom2="";
+        for(var i in sybmRes){
+    
+            if(z2==2) {  
+                sakom2+=sybmRes[i]; sybmRes2[n2]=sakom2; sakom2="";  z2=0; n2++; 
+            } else { 
+                sakom2+=sybmRes[i]; z2++; 
+                if(i==sybmRes.length-1) { sybmRes2[n2]=sakom2; } 
+            } 
+        } // end for 
+        
+        sybmRes=sybmRes2;  
+    
+    }// end if
+    
+    
+    for(var i in sybmRes){
+        var sumbcode=sybmRes[i];
+        result+=symb2to8[sumbcode];
+    } // end for 
+    
+  return result;
+} //end fun
+
+
+function convertNumber2to16(a){
+
+    var sla = a.toLowerCase();
+
+    var result="";
+    
+    var z=0;
+    var n=0;
+    var sybmRes=[];
+    var sakom="";
+    for(var i in sla){
+    
+        if(z==3) {  
+            sakom+=sla[i]; sybmRes[n]=sakom; sakom="";  z=0; n++; 
+        } else { 
+            sakom+=sla[i]; z++;
+            if(i==sla.length-1) { sybmRes[n]=sakom; }  
+        } 
+    } // end for
+    
+
+    if(z==1 || z==2 || z==3) {
+    
+       // из массива в строку
+       sybmRes=sybmRes.join(''); 
+    
+       if(z==1) { sybmRes="000"+sybmRes; }
+       if(z==2) { sybmRes="00"+sybmRes; }
+       if(z==3) { sybmRes="0"+sybmRes; }
+    
+        var z2=0;
+        var n2=0;
+        var sybmRes2=[];
+        var sakom2="";
+        for(var i in sybmRes){
+    
+            if(z2==3) {  
+                sakom2+=sybmRes[i]; sybmRes2[n2]=sakom2; sakom2="";  z2=0; n2++; 
+            } else { 
+                sakom2+=sybmRes[i]; z2++; 
+                if(i==sybmRes.length-1) { sybmRes2[n2]=sakom2; } 
+            } 
+        } // end for 
+        
+        sybmRes=sybmRes2;  
+    
+    }// end if
+    
+    
+    for(var i in sybmRes){
+        var sumbcode=sybmRes[i];
+        result+=symb2to16[sumbcode];
+    } // end for 
+    
+  return result;
+} //end fun
+
+function zeroPadding(str, razr) {
+
+    var keyM=razr-str.length;
+  
+    var mr="";
+    var z=0;
+    for(var i=1;i<=razr;i++) {
+        if(i>keyM) { 
+            mr+=str[z]; 
+            z++;
+        } else { 
+            mr+='0'; 
+        }
+    } // end for
+  return mr;
+} // end fun
